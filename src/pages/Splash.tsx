6bcd0d1b49789aa1c91afdf01f2a5cf03f901cfe
Nam-Wijeong <dnlwjdv@hanmail.net>
2022-12-22 00:24:42 +0900

    :sparkles: Feat : 스플래시 페이지 추가

--- src/pages/Splash.tsx
+++ src/pages/Splash.tsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
+import BarLoader from "react-spinners/BarLoader";
+import { useNavigate } from 'react-router';
+import '../sass/pages/_splash.scss';
 
 const Splash = () => {
-  return <div>Splash</div>;
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setTimeout(() => {
+      navigate('/login')
+    }, 1500)
+  }, [])
+
+  return (
+    <div className='wrapper'>
+      <div>
+        <h1 className='title'>2023</h1>
+        <BarLoader
+          width={200}
+          height={14}
+          color={'#ffffff'}
+          speedMultiplier={0.6}
+        />
+        <p className='loadingMsg'>LOADING . . .</p>
+      </div>
+    </div>
+  );
 };
 
 export default Splash;
