7526245704c2192cb5865096f85425eb58c66be9
Hyebin-Woo <dev.hyebin@gmail.com>
2022-12-21 16:52:07 +0900

    :lipstick: Add: scss폴더 구조 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
+import React from "react";
+import "./sass/main.scss";
 
 function App() {
-  return (
-    <div></div>
-  );
+  return <div></div>;
 }
 
 export default App;
