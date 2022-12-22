edf146c17d3bfd431083366d14ea7217669e513a
Hyebin-Woo <dev.hyebin@gmail.com>
2022-12-22 22:52:44 +0900

    :sparkles: Add:뒤로가기아톰

--- src/atoms/Backspace.tsx
+++ src/atoms/Backspace.tsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Backspace = () => {
-  return <div>Backspace</div>;
+  const navigate = useNavigate();
+  return (
+    <button
+      onClick={() => {
+        navigate(-1);
+      }}
+      aria-label="뒤로가기버튼"
+    >
+      <img
+        src={process.env.PUBLIC_URL + "assets/images/icon-back.svg"}
+        alt="뒤로가기이미지"
+      />
+    </button>
+  );
 };
 
 export default Backspace;
