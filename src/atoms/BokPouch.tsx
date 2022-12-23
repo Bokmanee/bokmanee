13157fea819d3595fd02f5a614181a49a1527508
ujin16 <94890646+ujin16@users.noreply.github.com>
2022-12-23 17:50:11 +0900

    :sparkles: Feat: 복주머니 아톰 마크업 작업
    git push -u origin Feat/22/Atome-BokPouch

--- src/atoms/BokPouch.tsx
+++ src/atoms/BokPouch.tsx
@@ -1,7 +1,19 @@
 import React from "react";
 
-const BokPouch = () => {
-  return <div>BokPouch</div>;
+interface BokPouchProps {
+  color: string;
+  angle?: string;
+  nickname: string;
+}
+
+const BokPouch = ({ color, angle, nickname }: BokPouchProps) => {
+  return (
+    <button className="bokPouch flex-align">
+      <span className="ir">메세지 확인하기</span>
+      <div className={`pouch-${color} pouch-${angle ?? angle}`} />
+      <label>{nickname}</label>
+    </button>
+  );
 };
 
 export default BokPouch;
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,10 +1,9 @@
 import React from "react";
-import { Button, GitHubButton, KakaoButton } from "../atoms/Button";
+import { GitHubButton, KakaoButton } from "../atoms/Button";
 
 const Login = () => {
   return (
     <div>
-      <Button>버튼</Button>
       <KakaoButton>카카오</KakaoButton>
       <GitHubButton>깃허브</GitHubButton>
     </div>
