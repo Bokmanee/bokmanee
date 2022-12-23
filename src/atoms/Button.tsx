246104538a7fe2cee3e8af17fd1a0dace664a4d5
ujin16 <94890646+ujin16@users.noreply.github.com>
2022-12-23 13:26:45 +0900

    :sparkles: Feat: Button 아톰 구현

--- src/atoms/Button.tsx
+++ src/atoms/Button.tsx
@@ -1,7 +1,45 @@
 import React from "react";
 
-const Button = () => {
-  return <div>Button</div>;
+export interface BtnProps {
+  type?: "submit" | "button";
+  onClick?: () => void;
+  children?: React.ReactNode;
+}
+
+const WhButton = ({ type, onClick, children }: BtnProps) => {
+  return (
+    <button className="btn-wh" type={type ?? type} onClick={onClick ?? onClick}>
+      {children}
+    </button>
+  );
 };
 
-export default Button;
+const SkyButton = ({ type, onClick, children }: BtnProps) => {
+  return (
+    <button
+      className="btn-sky"
+      type={type ?? type}
+      onClick={onClick ?? onClick}
+    >
+      {children}
+    </button>
+  );
+};
+
+const KakaoButton = ({ onClick, children }: BtnProps) => {
+  return (
+    <button className="btn-kakao" type="button" onClick={onClick ?? onClick}>
+      {children}
+    </button>
+  );
+};
+
+const GitHubButton = ({ onClick, children }: BtnProps) => {
+  return (
+    <button className="btn-github" type="button" onClick={onClick ?? onClick}>
+      {children}
+    </button>
+  );
+};
+
+export { WhButton, SkyButton, KakaoButton, GitHubButton };
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { Button, GitHubButton, KakaoButton } from "../atoms/Button";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div>
+      <Button>버튼</Button>
+      <KakaoButton>카카오</KakaoButton>
+      <GitHubButton>깃허브</GitHubButton>
+    </div>
+  );
 };
 
 export default Login;
