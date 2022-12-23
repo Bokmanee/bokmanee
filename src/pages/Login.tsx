12e54764a2c6be257693163f53551dc0c2092fb9
Hyebin-Woo <dev.hyebin@gmail.com>
2022-12-24 00:39:38 +0900

    :lipstick: Add: 로그인페이지 마크업 구현

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,12 +1,37 @@
 import React from "react";
-import { WhButton, GitHubButton, KakaoButton } from "../atoms/Button";
+import { Link } from "react-router-dom";
+import {
+  WhButton,
+  GitHubButton,
+  KakaoButton,
+  SkyButton,
+} from "../atoms/Button";
+import "../sass/pages/_login.scss";
+import Input from "../atoms/Input";
+import Header from "../components/Header";
+import BokClick from "../atoms/BokClick";
 
 const Login = () => {
   return (
-    <div>
-      <WhButton>버튼</WhButton>
-      <KakaoButton>카카오</KakaoButton>
-      <GitHubButton>깃허브</GitHubButton>
+    <div className="wrap">
+      <Header rightChild={<BokClick />} />
+      <section>
+        <h1 className="ir">로그인</h1>
+        <h4 className="txt-intro">HAPPY 2023 !</h4>
+        <h2 className="txt-title">우리 모두 복만이</h2>
+        <Input type="text" label="이메일" placeholder="이메일" />
+        <Input type="password" label="비밀번호" placeholder="비밀번호." />
+        <WhButton>로그인</WhButton>
+        <Link className="link-passfind" to={"/findMyPassword"}>
+          비밀번호를 잊으셨나요?
+        </Link>
+
+        <div className="list-join">
+          <KakaoButton>카카오</KakaoButton>
+          <GitHubButton>깃허브</GitHubButton>
+          <SkyButton>복만이 시작하기</SkyButton>
+        </div>
+      </section>
     </div>
   );
 };
