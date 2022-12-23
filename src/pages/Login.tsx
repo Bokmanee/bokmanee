b227001275098862ba4b87e61baa407b01126f86
leeyeun <dpdms0109@naver.com>
2022-12-23 17:05:53 +0900

    :sparkles: Feat : 회원가입 페이지 마크업

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { Button, GitHubButton, KakaoButton } from "../atoms/Button";
+import { WhButton, GitHubButton, KakaoButton } from "../atoms/Button";
 
 const Login = () => {
   return (
     <div>
-      <Button>버튼</Button>
+      <WhButton>버튼</WhButton>
       <KakaoButton>카카오</KakaoButton>
       <GitHubButton>깃허브</GitHubButton>
     </div>
