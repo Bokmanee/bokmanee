2236c972113ae1dcd1961bcbf161a84d000af8cf
Hyebin-Woo <dev.hyebin@gmail.com>
2022-12-25 23:11:02 +0900

    :lipstick: Add: 로그인 마크업 추가

--- src/pages/MyBoard.tsx
+++ src/pages/MyBoard.tsx
@@ -1,7 +1,38 @@
 import React from "react";
+import "../sass/pages/_myBoard.scss";
+import Header from "../components/Header";
+import BokClick from "../atoms/BokClick";
+import Board from "../atoms/Board";
+import BokPouch from "../atoms/BokPouch";
+import { WhButton } from "../atoms/Button";
 
 const MyBoard = () => {
-  return <div>MyBoard</div>;
+  return (
+    <>
+      <Header rightChild={<BokClick />} />
+      <div className="wrap-myboard">
+        <Board
+          username="옌니"
+          message1="님이 받은"
+          message2="새해 응원 메시지를 확인해보세요  !"
+        />
+        <div className="grid-bok">
+          <BokPouch color="red" nickname="전갈진" />
+          <BokPouch color="purple" nickname="전갈진" />
+          <BokPouch color="red" nickname="전갈진" />
+          <BokPouch color="red" nickname="전갈진" />
+          <BokPouch color="blue" nickname="전갈진" />
+          <BokPouch color="red" nickname="전갈진" />
+          <BokPouch color="yellow" nickname="전갈진" />
+          <BokPouch color="yellow" nickname="전갈진" />
+          <BokPouch color="yellow" nickname="전갈진" />
+          <BokPouch color="yellow" nickname="전갈진" />
+          <BokPouch color="yellow" nickname="전갈진" />
+        </div>
+        <WhButton>링크공유하기</WhButton>
+      </div>
+    </>
+  );
 };
 
 export default MyBoard;
