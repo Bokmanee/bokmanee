2622cf08b428b5aa1bd6d188298da427e6a22a94
Hyebin-Woo <dev.hyebin@gmail.com>
2022-12-25 23:10:28 +0900

    :lipstick: Modify: 보드 아톰 수정

--- src/atoms/Board.tsx
+++ src/atoms/Board.tsx
@@ -1,8 +1,5 @@
-import React from 'react'
-import Backspace from './Backspace';
-import BokClick from './BokClick';
-import Header from '../components/Header';
-import "../sass/atoms/_board.scss"
+import React from "react";
+import "../sass/atoms/_board.scss";
 
 interface BoardProps {
   username?: string;
@@ -13,17 +10,16 @@ interface BoardProps {
 const Board = ({ username, message1, message2 }: BoardProps) => {
   return (
     <>
-      <Header
-        leftChild={<Backspace />}
-        rightChild={<BokClick />} />
-      <div className='board-wrapper'>
-        <h1 className='board-title'>
-          <span className='username'>{username}</span>
-          {message1}<br />
-          {message2}</h1>
+      <div className="board-wrapper">
+        <h1 className="board-title">
+          <span className="username">{username}</span>
+          {message1}
+          <br />
+          {message2}
+        </h1>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Board
+export default Board;
